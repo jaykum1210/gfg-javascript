bcc99a4b006bc03aeebd7cdc0031c6e3b105ee3d
jaykum1210 <jaykumawat1210@gmail.com>
2025-10-05 15:20:08 +0530

    add some files

---
+++ basics of js/2.js
@@ -0,0 +1,4 @@
+console.log("hello Everone");
+console.log("My name is Jay Kumawat");
+
+console.log(7+3);
---
+++ basics of js/3.js
@@ -0,0 +1,13 @@
+var a = 1000;
+console.log(a);
+
+var b = "Hello World!";
+console.log(b);
+
+console.log(a + ' ' +b);
+
+a = 20;
+console.log(a);
+
+let c = "hello";
+console.log(c);
